bda870561b25561c1063dd4443ccfb8d66ff276a
judah-b2 <masterofcoding360@gmail.com>
2021-09-10 14:22:38 +0530

    deleted:    ignout 	modified:   main.py 	new file:   sprites/imageSelect.png 	new file:   sprites/imageSelected.png

--- main.py
+++ main.py
@@ -8,13 +8,26 @@ pygame.display.set_caption("CTM maker")
 
 running = True
 
+class Scene:
+	def __init__(self, surface):
+		self.surface = surface
+		self.surface.fill((60,60,60,255))
+
+	def draw(self):
+		self.surface.fill((60,60,60,255))
+
+	def update(self):
+		self.draw()
+
+scene = Scene(window)
+
 while running:
 	events = pygame.event.get()
 	for event in events:
 		if event.type == pygame.QUIT:
 			running = False
 
-	window.fill((60,60,60,255))
+	scene.update()
 	pygame.display.flip()
 
 pygame.quit()
