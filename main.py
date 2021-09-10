5cc60c0e49fe95580367d35de6842a613863265d
judah-b2 <masterofcoding360@gmail.com>
2021-09-10 14:14:14 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -3,4 +3,18 @@ from ctm.Constants import *
 import pygame
 
 pygame.init()
-window = pygame.display.set_mode(200, 300)
+window = pygame.display.set_mode((600, 500))
+pygame.display.set_caption("CTM maker")
+
+running = True
+
+while running:
+	events = pygame.event.get()
+	for event in events:
+		if event.type == pygame.QUIT:
+			running = False
+
+	window.fill((60,60,60,255))
+	pygame.display.flip()
+
+pygame.quit()
